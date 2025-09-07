4b3a3c417a36f94d039bf6da252e7144eb88b413
Sami Alabed <97133330+Quazar01@users.noreply.github.com>
2025-09-07 16:05:12 +0200

    Add 1295 röda backar

--- app.js
+++ app.js
@@ -253,6 +253,7 @@ const products = [
   new Product("Not Set", 1292, red),
   new Product("Not Set", 1293, red),
   new Product("Not Set", 1294, red),
+  new Product("Not Set", 1295, red),
   new Product("Not Set", 1361, red),
   new Product("Not Set", 1391, red),
   new Product("Not Set", 1411, red),
